8567438360d7d465881290af4dcf343a82c26092
pc aquio600 <lavanantclement23@gmail.com>
2025-01-10 11:08:34 +0100

    suiviprojet

--- src/main/java/com/example/tp8_exercice1/SuiviDesProjets.java
+++ src/main/java/com/example/tp8_exercice1/SuiviDesProjets.java
@@ -2,4 +2,34 @@ package com.example.tp8_exercice1;
 import java.util.ArrayList;
 
 public class SuiviDesProjets {
+    private int suiviProjet;
+    private int idProjetSuivi;
+    private Enum avancementProjet;
+
+    public SuiviDesProjets(Enum avancementProjet, int idProjetSuivi, int suiviProjet) {
+        this.avancementProjet = avancementProjet;
+        this.idProjetSuivi = idProjetSuivi;
+        this.suiviProjet = suiviProjet;
+    }
+
+    public Enum getAvancementProjet() {return avancementProjet;}
+
+    public void setAvancementProjet(Enum avancementProjet) {this.avancementProjet = avancementProjet;}
+
+    public int getIdProjetSuivi() {return idProjetSuivi;}
+
+    public void setIdProjetSuivi(int idProjetSuivi) {
+        this.idProjetSuivi = idProjetSuivi;
+    }
+
+    public int getSuiviProjet() {
+        return suiviProjet;
+    }
+
+    public void setSuiviProjet(int suiviProjet) {
+        this.suiviProjet = suiviProjet;
+    }
+    public void afficherToutLesProjets(){}
+    public void isAdmin(){}
+    public void voirAvancementProjet(){}
 }
